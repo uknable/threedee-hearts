5d722869f388827f5d17e41898947760c64ca845
uknable <uknable@hotmail.com>
2020-04-05 14:05:33 +1000

    firebase

---
+++ init-firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDovCwjQnKqKOpHPfWfSRyx1vwQsB4dxvY",
+    authDomain: "threedee-hearts.firebaseapp.com",
+    databaseURL: "https://threedee-hearts.firebaseio.com",
+    projectId: "threedee-hearts",
+    storageBucket: "threedee-hearts.appspot.com",
+    messagingSenderId: "87105317899",
+    appId: "1:87105317899:web:974c590dfa10ce841fab0e"
+};
+
+
+firebase.initializeApp(firebaseConfig);
