813ae6b7def1646867ca7aecce42fbca8509d98f
uknable <uknable@hotmail.com>
2020-04-28 14:16:16 +1000

    testing fetch from storyblok url

--- server.js
+++ server.js
@@ -2,6 +2,27 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 4000;
+const StoryblokClient = require('storyblok-js-client');
+// import StoryblokClient from 'storyblok-js-client';
+
+const Storyblok = new StoryblokClient({
+    accessToken: 'cxBOVdDowPMBH7h41jTGuQtt',
+    cache: {
+        clear: 'auto',
+        type: 'memory'
+    }
+});
+
+Storyblok.get('cdn/stories', {
+    "token": "wANpEQEsMYGOwLxwXQ76Ggtt"
+  })
+  .then(response => {
+    console.log(response)
+  }).catch(error => { 
+    console.log(error)
+  })
+
+console.log("hello require");
 
 app.use(express.static(__dirname + '/public'));
 
